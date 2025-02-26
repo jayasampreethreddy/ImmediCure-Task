5e77647f8a1402d32d5044be98da2d43740c219a
Jaya Sampreeth Reddy <kondejayasampreethreddy@gmail.com>
2025-02-26 20:23:23 +0530

    prepare for deployment

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template, request
 from models.keymapping import search_doctors
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app) 
+
 
 @app.route('/')
 def home():
