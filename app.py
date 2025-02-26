244058c6a655aa81e244ee8a26819a19851ffe8f
Jaya Sampreeth Reddy <kondejayasampreethreddy@gmail.com>
2025-02-26 22:30:15 +0530

    procfile added

--- app.py
+++ app.py
@@ -24,4 +24,5 @@ def search():
 
     return render_template('results.html', doctors=doctors)
 if __name__ == '__main__':
-    app.run(debug=True)
+    if __name__ == '__main__':
+        app.run(host="0.0.0.0", port=5000)
