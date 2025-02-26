24e2585e797dfc2a77b86fb6f8bb7d7188994cec
Jaya Sampreeth Reddy <kondejayasampreethreddy@gmail.com>
2025-02-26 23:06:50 +0530

    commit_v2

--- app.py
+++ app.py
@@ -24,5 +24,4 @@ def search():
 
     return render_template('results.html', doctors=doctors)
 if __name__ == '__main__':
-    if __name__ == '__main__':
-        app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000)
